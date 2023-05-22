ec195313bf614ea1212f75c4177b90a77f99f5fe
ronman194 <mansharofron@gmail.com>
2023-05-22 10:50:53 +0300

    style

--- pages/products/new.tsx
+++ pages/products/new.tsx
@@ -3,7 +3,14 @@ import Layout from "@/components/Layout";
 export default function NewProduct() {
     return (
         <Layout>
-            New product
+            <h1>New Product</h1>
+            <label>Product Name</label>
+            <input type="text" placeholder="Product Name" />
+            <label>Description</label>
+            <textarea placeholder="Description"></textarea>
+            <label>Price</label>
+            <input type="number" placeholder="Price" />
+            <button className="btn-primary">Save</button>
         </Layout>
     )
 }
